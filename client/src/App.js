6d1249936c31f0dc4cd7c8f3e44c1a9e609b8f1a
Ricky Zaman <ricky.zaman@outlook.com>
2021-10-25 17:32:39 -0400

    initial rescope

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & MERN Stack & Auth
+          Zocially Coming Soon
         </a>
       </header>
     </div>
